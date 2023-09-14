240078cc34613597f088da25b3e67d807569edfb
Abhishek Soren <abhisheksoren95@gmail.com>
2023-09-14 18:58:41 +0530

    squirrel count csv

--- squirrel_count/main.py
+++ squirrel_count/main.py
@@ -6,8 +6,22 @@ color = data["Primary Fur Color"]
 color_list = color.to_list()
 
 gray_count = color_list.count("Gray")
+
+#other way to get the number of gray squirrels
+gray_count_new = len(data[data["Primary Fur Color"] == "Gray"])
+print(gray_count_new)
+
+
 cinnamon_count = color_list.count("Cinnamon")
+
+cinnamon_count_new = len(data[data["Primary Fur Color"] == "Cinnamon"])
+print(cinnamon_count_new)
+
 black_count = color_list.count("Black")
+
+black_count_new = len(data[data["Primary Fur Color"] == "Black"])
+print(black_count_new)
+
 print(gray_count, cinnamon_count, black_count)
 
 new_color_list = {
